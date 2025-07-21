ee5b53a20af35908154500e086649895f8ad9450
RedCrown394 <renzolim3@gmail.com>
2025-07-21 19:17:04 +0800

    This is a commit

--- FirstGitHubUpload/Form1.Designer.vb
+++ FirstGitHubUpload/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Button1 = New Button()
+        Button2 = New Button()
         SuspendLayout()
         ' 
         ' Button1
@@ -34,11 +35,21 @@ Partial Class Form1
         Button1.Text = "Button1"
         Button1.UseVisualStyleBackColor = True
         ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(357, 196)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(228, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
@@ -46,5 +57,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 
 End Class
